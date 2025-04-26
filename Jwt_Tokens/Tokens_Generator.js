153a0cd67f460bfc564b7022686f91465ce49ecd
TeepakornPC <teeprakorn.toey@gmail.com>
2025-04-26 18:45:03 +0700

    VERSION 0.1.0

--- Jwt_Tokens/Tokens_Generator.js
+++ Jwt_Tokens/Tokens_Generator.js
@@ -1,21 +1,23 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-//Generator Token
+// Generator Token
 function Tokens_Generator(Users_ID, Users_Email, Users_Username, UsersType_ID, Users_Type) {
-    if(!Users_ID || !Users_Email || !Users_Username || !UsersType_ID || !Users_Type ){ return 0;}else{
-        let Token;
-          Token = jwt.sign(
-          {
-            Users_ID: req.Users_decoded.Users_ID,
-            Users_Email: req.Users_decoded.Users_Email,
-            Users_Username: req.Users_decoded.Users_Username,
-            UsersType_ID: req.Users_decoded.UsersType_ID,
-            Users_Type: req.Users_decoded.Users_Type
-          },
-          process.env.PRIVATE_TOKEN_KEY,{ expiresIn: '24h'}
-        );
-        return Token;
-      };
-    };
+  if (!Users_ID || !Users_Email || !Users_Username || !UsersType_ID || !Users_Type) {
+    return 0;
+  } else {
+    const Token = jwt.sign(
+      {
+        Users_ID: Users_ID,
+        Users_Email: Users_Email,
+        Users_Username: Users_Username,
+        UsersType_ID: UsersType_ID,
+        Users_Type: Users_Type
+      },
+      process.env.PRIVATE_TOKEN_KEY, 
+      { expiresIn: '24h' }
+    );
+    return Token;
+  }
+}
 
-  module.exports = Tokens_Generator;
+module.exports = Tokens_Generator;
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ require('dotenv').config();
 const loginRateLimiter = require('./Rate_Limiter/LimitTime_Login');
 const GenerateTokens = require('./Jwt_Tokens/Tokens_Generator');
 const VerifyTokens = require('./Jwt_Tokens/Tokens_Verification');
+const e = require('express');
 
 const app = express();
 const saltRounds = 14;
@@ -113,6 +114,75 @@ app.post('/api/verifyToken', VerifyTokens, (req, res) => {
   }
   return res.status(402).json({ message: 'Invalid Token.', status: false });
 });
+
+//////////////////////////////////Login API///////////////////////////////////////
+//API Login
+app.post('/api/login', loginRateLimiter, async (req, res) => {
+  let { Users_Email, Users_Password } = req.body;
+
+  if (!Users_Email || !Users_Password ||
+    typeof Users_Email !== 'string' || typeof Users_Password !== 'string') {
+      return res.status(400).json({ message: 'Please fill in the correct parameters as required.', status: false });
+  }
+
+  Users_Email = xss(validator.escape(Users_Email));
+  Users_Password = xss(validator.escape(Users_Password));
+
+  const sql_check_username = "SELECT COUNT(*) AS count FROM users WHERE Users_Email = ? AND Users_IsActive = 1";
+  db.query(sql_check_username, [Users_Email], async (err, result) => {
+    if (err) { return res.status(500).json({ message: 'An error occurred on the server.', status: false }); }
+
+    if (result[0].count > 0) {
+      const sql_get_password = "SELECT Users_Password FROM users WHERE Users_Email = ? AND Users_IsActive = 1";
+      db.query(sql_get_password, [Users_Email], async (err, result) => {
+        if (err) { return res.status(500).json({ message: 'An error occurred on the server.', status: false }); }
+
+        const isCorrect = await bcrypt.compare(Users_Password, result[0].Users_Password);
+        if (isCorrect) {
+          const sql = "SELECT * FROM users WHERE Users_Email = ? AND Users_IsActive = 1";
+          db.query(sql, [Users_Email], async (err, result) => {
+            if (err) { return res.status(500).json({ message: 'An error occurred on the server.', status: false });}
+            const users_results = result[0];
+            const Users = {};
+            let sql_users_type, users_type_name_id;
+            if (users_results.Users_Type === 'student') {
+              sql_users_type = "SELECT * FROM student WHERE Users_ID = ?";
+              users_type_name_id = 'Student_ID';
+            }else if (users_results.Users_Type === 'teacher') {
+              sql_users_type = "SELECT * FROM teacher WHERE Users_ID = ?";
+              users_type_name_id = 'Teacher_ID';
+            }else if (users_results.Users_Type === 'staff') {
+              sql_users_type = "SELECT * FROM staff WHERE Users_ID = ?";
+              users_type_name_id = 'Staff_ID';
+            }else{
+              return res.status(400).json({ message: 'Invalid user type.', status: false });
+            }
+
+            db.query(sql_users_type, [users_results.Users_ID], async (err, result) => {
+              if (err) { return res.status(500).json({ message: 'An error occurred on the server.', status: false }); }
+              const users_type_results = result[0];
+              const UsersType_ID = users_type_results[users_type_name_id]
+
+              const Tokens = GenerateTokens(users_results.Users_ID,
+                users_results.Users_Email, users_results.Users_Username, UsersType_ID, users_results.Users_Type);
+              // const Tokens = GenerateTokens('1','2','3','4','5');
+              Users['token'] = Tokens;
+              Users['message'] = "The password is correct."
+              Users['status'] = true
+              res.status(200).send(Users);
+            });
+          });
+        } else {
+          res.status(201).json({ message: "The password is incorrect.", status: false });
+        }
+      });
+    } else {
+      res.status(202).json({ message: "The password is incorrect.", status: false });
+    }
+  });
+});
+
+/////////////////////////////////////////////////////////////////////////
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Example app listening on port ${process.env.SERVER_PORT}`)
