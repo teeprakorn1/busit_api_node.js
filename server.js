079f7fc4e156ac83fcf4bffdd4982ce599dc027e
TeepakornPC <teeprakorn.toey@gmail.com>
2025-04-26 19:08:55 +0700

    VERSION 0.1.1

--- server.js
+++ server.js
@@ -56,15 +56,19 @@ const protectSwagger = (req, res, next) => {
 };
 
 // Apply Swagger Middleware
-if (process.env.NODE_ENV === '1') { // Production
+if (process.env.NODE_ENV === '1') { // (Production)
   app.use('/api-docs', protectSwagger, swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
-} else { // Development
+} else { // (Development)
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
 }
 
 ////////////////////////////////// TEST API ///////////////////////////////////////
 // Encrypt Test
 app.post('/api/test/encrypt', async (req, res) => {
+  if (process.env.NODE_ENV === '0') {
+    return res.status(403).json({ message: 'This API is not allowed in production.', status: false }); 
+  }
+
   try {
     const { password } = req.body;
     if (!password) {
@@ -80,6 +84,10 @@ app.post('/api/test/encrypt', async (req, res) => {
 
 // Decrypt Test
 app.post('/api/test/decrypt', async (req, res) => {
+  if (process.env.NODE_ENV === '0') {
+    return res.status(403).json({ message: 'This API is not allowed in production.', status: false }); 
+  }
+
   try {
     const { password, hash } = req.body;
     if (!password || !hash) {
@@ -116,70 +124,79 @@ app.post('/api/verifyToken', VerifyTokens, (req, res) => {
 });
 
 //////////////////////////////////Login API///////////////////////////////////////
-//API Login
-app.post('/api/login', loginRateLimiter, async (req, res) => {
+//API Login Application
+app.post('/api/login/application', loginRateLimiter, async (req, res) => {
   let { Users_Email, Users_Password } = req.body;
 
   if (!Users_Email || !Users_Password ||
     typeof Users_Email !== 'string' || typeof Users_Password !== 'string') {
-      return res.status(400).json({ message: 'Please fill in the correct parameters as required.', status: false });
+    return res.status(400).json({ message: 'Please fill in the correct parameters as required.', status: false });
   }
 
   Users_Email = xss(validator.escape(Users_Email));
   Users_Password = xss(validator.escape(Users_Password));
 
-  const sql_check_username = "SELECT COUNT(*) AS count FROM users WHERE Users_Email = ? AND Users_IsActive = 1";
-  db.query(sql_check_username, [Users_Email], async (err, result) => {
-    if (err) { return res.status(500).json({ message: 'An error occurred on the server.', status: false }); }
+  try {
+    const sql = "SELECT * FROM users WHERE Users_Email = ? AND Users_IsActive = 1";
+    db.query(sql, [Users_Email], async (err, result) => {
+      if (err) { 
+        console.error('Database error (users)', err);
+        return res.status(500).json({ message: 'An error occurred on the server.', status: false }); 
+      }
 
-    if (result[0].count > 0) {
-      const sql_get_password = "SELECT Users_Password FROM users WHERE Users_Email = ? AND Users_IsActive = 1";
-      db.query(sql_get_password, [Users_Email], async (err, result) => {
-        if (err) { return res.status(500).json({ message: 'An error occurred on the server.', status: false }); }
+      if (result.length === 0) {
+        return res.status(401).json({ message: "The password is incorrect.", status: false });
+      }
 
-        const isCorrect = await bcrypt.compare(Users_Password, result[0].Users_Password);
-        if (isCorrect) {
-          const sql = "SELECT * FROM users WHERE Users_Email = ? AND Users_IsActive = 1";
-          db.query(sql, [Users_Email], async (err, result) => {
-            if (err) { return res.status(500).json({ message: 'An error occurred on the server.', status: false });}
-            const users_results = result[0];
-            const Users = {};
-            let sql_users_type, users_type_name_id;
-            if (users_results.Users_Type === 'student') {
-              sql_users_type = "SELECT * FROM student WHERE Users_ID = ?";
-              users_type_name_id = 'Student_ID';
-            }else if (users_results.Users_Type === 'teacher') {
-              sql_users_type = "SELECT * FROM teacher WHERE Users_ID = ?";
-              users_type_name_id = 'Teacher_ID';
-            }else if (users_results.Users_Type === 'staff') {
-              sql_users_type = "SELECT * FROM staff WHERE Users_ID = ?";
-              users_type_name_id = 'Staff_ID';
-            }else{
-              return res.status(400).json({ message: 'Invalid user type.', status: false });
-            }
+      const user = result[0];
+      const passwordMatch = await bcrypt.compare(Users_Password, user.Users_Password);
+      if (!passwordMatch) {
+        return res.status(401).json({ message: "The password is incorrect.", status: false });
+      }
 
-            db.query(sql_users_type, [users_results.Users_ID], async (err, result) => {
-              if (err) { return res.status(500).json({ message: 'An error occurred on the server.', status: false }); }
-              const users_type_results = result[0];
-              const UsersType_ID = users_type_results[users_type_name_id]
+      // Check Users_Type
+      let sql_users_type, users_type_name_id;
+      if (user.Users_Type === 'student') {
+        sql_users_type = "SELECT * FROM student WHERE Users_ID = ?";
+        users_type_name_id = 'Student_ID';
+      } else if (user.Users_Type === 'teacher') {
+        sql_users_type = "SELECT * FROM teacher WHERE Users_ID = ?";
+        users_type_name_id = 'Teacher_ID';
+      } else if (user.Users_Type === 'staff') {
+        sql_users_type = "SELECT * FROM staff WHERE Users_ID = ?";
+        users_type_name_id = 'Staff_ID';
+      } else {
+        return res.status(400).json({ message: 'Invalid user type.', status: false });
+      }
 
-              const Tokens = GenerateTokens(users_results.Users_ID,
-                users_results.Users_Email, users_results.Users_Username, UsersType_ID, users_results.Users_Type);
-              // const Tokens = GenerateTokens('1','2','3','4','5');
-              Users['token'] = Tokens;
-              Users['message'] = "The password is correct."
-              Users['status'] = true
-              res.status(200).send(Users);
-            });
-          });
-        } else {
-          res.status(201).json({ message: "The password is incorrect.", status: false });
+      db.query(sql_users_type, [user.Users_ID], async (err, result_users_type) => {
+        if (err) {
+          console.error('Database error (user type)', err);
+          return res.status(500).json({ message: 'An error occurred on the server.', status: false });
         }
+
+        if (result_users_type.length === 0) {
+          return res.status(404).json({ message: 'User type details not found.', status: false });
+        }
+
+        const userType = result_users_type[0];
+        const UsersType_ID = userType[users_type_name_id];
+
+        const token = GenerateTokens(user.Users_ID, 
+          user.Users_Email, user.Users_Username, UsersType_ID, user.Users_Type);
+
+        const responseData = {
+          token: token,
+          message: "The login was successful.",
+          status: true
+        };
+        res.status(200).json(responseData);
       });
-    } else {
-      res.status(202).json({ message: "The password is incorrect.", status: false });
-    }
-  });
+    });
+  } catch (error) {
+    console.error('Catch error', error);
+    res.status(500).json({ message: 'An unexpected error occurred.', status: false });
+  }
 });
 
 /////////////////////////////////////////////////////////////////////////
