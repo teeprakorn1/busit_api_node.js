b8dc7cf537ddf9fe6a65622eb80753e486a48a9d
TeepakornPC <teeprakorn.toey@gmail.com>
2025-08-21 21:58:22 +0700

    VERSION 0.3.5.1

--- Server_Services/databaseClient.js
+++ Server_Services/databaseClient.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
-//MySQL Connection
 const db = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
@@ -8,21 +8,18 @@ const db = mysql.createPool({
   database: process.env.DATABASE_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.getConnection((err) => {
   if (err) {
-    console.error(' Database connection error:', err.code);
-    
-    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
-      return res.status(503).json({ message: 'เชื่อมต่อฐานข้อมูลไม่สำเร็จ กรุณาลองใหม่อีกครั้ง' });
-    }
-
-    return res.status(500).json({ message: 'เกิดข้อผิดพลาดในการเชื่อมต่อฐานข้อมูล' });
+    console.error('Database connection error:', err.code);
+    return;
   }
+  console.log('Database connected successfully with SSL');
 });
-
-console.log('Database connected successfully');
 
 module.exports = db;
