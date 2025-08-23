b0eab2c787ab2a23488b7377de949bd2ab55a918
TeepakornPC <teeprakorn.toey@gmail.com>
2025-08-23 21:11:22 +0700

    VERSION 0.3.5.2

--- Server_Services/databaseClient.js
+++ Server_Services/databaseClient.js
@@ -6,6 +6,7 @@ const db = mysql.createPool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASS,
   database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
